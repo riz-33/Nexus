f5961445f5f8c68f6af4f1530431a1d29cb41aa2
muhammad <rizwan.quettawala@gmail.com>
2025-12-11 18:10:09 +0500

    adding calendar

---
+++ src/pages/calendar/MeetingsPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MeetingCalendar from "../../components/calendar/MeetingCalendar";
+
+export const MeetingsPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-gray-900">Meeting Calendar</h1>
+
+      <MeetingCalendar />
+    </div>
+  );
+};
